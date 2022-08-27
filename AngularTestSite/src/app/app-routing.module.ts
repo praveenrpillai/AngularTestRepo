dedc91aaffd29e07b1516adb86e3266c2014087d
Adish P Pillai <adish.pillai@hotmail.com>
2022-08-28 00:57:00 +0530

    update

--- AngularTestSite/src/app/app-routing.module.ts
+++ AngularTestSite/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './components/log-in/log-in.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LogInComponent },
+  { path: 'departments', component: DepartmentListComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'products', component: ProductDetailsComponent }
  ];
 
 @NgModule({
@@ -13,3 +19,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [DepartmentListComponent, EmployeeListComponent, LogInComponent, ProductDetailsComponent]
